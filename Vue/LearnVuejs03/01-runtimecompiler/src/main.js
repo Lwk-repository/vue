59a7b01afd0bfcea84a7f2e9731c6a334e84c260
lwk <2049808824@qq.com>
2020-12-11 10:49:37 +0800

    CLI3

--- Vue/LearnVuejs03/01-runtimecompiler/src/main.js
+++ Vue/LearnVuejs03/01-runtimecompiler/src/main.js
@@ -1,13 +1,14 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  render: function (h) {
+    // 1.普通用法：自己写标签 h('标签', {标签的属性}, [''])
+    // return h('h2', {class: 'box'}, ['hello world'])
+    // 2.传组件对象
+    return h(App);
+  }
 })
--- Vue/LearnVuejs03/02-runtimeonly/src/main.js
+++ Vue/LearnVuejs03/02-runtimeonly/src/main.js
@@ -3,8 +3,18 @@ import App from './App'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+// runtime-only(v2)(1.性能更高 2.下面的代码量更少)
+// render -> vdom -> UI
+// 比下面的方式轻6KB
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+// runtime-compiler(v1)
+// template -> ast -> render -> vdom -> UI
+// new Vue({
+//   el: '#app',
+//   template: '<App/>',
+//   components: { App }
+// })
