9a9a3b2bd3dce9529e2b0b5db27ccc817ab2a909
lwk <2049808824@qq.com>
2020-12-26 14:35:22 +0800

    页面完成

--- Vue/LearnVuejs02/02-webpack的配置/src/main.js
+++
@@ -1,3 +0,0 @@
-const {add, mul} = require('./js/mathUtils.js');
-console.log(add(11, 22));
-console.log(mul(33, 44));
---
+++ Vue/TabBar/tabbar/src/components/MainTabBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <!--    插槽实现-->
+  <tab-bar>
+    <tab-bar-item path="/home" activeColor="cornflowerblue">
+      <!--        具名插槽-->
+      <img src="../assets/img/tabbar/home.svg" slot="item-icon">
+      <img src="../assets/img/tabbar/home_active.svg" slot="item-icon-active">
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category" activeColor="cornflowerblue">
+      <img src="../assets/img/tabbar/category.svg" slot="item-icon">
+      <img src="../assets/img/tabbar/category_active.svg" slot="item-icon-active">
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart" activeColor="cornflowerblue">
+      <img src="../assets/img/tabbar/shopcart.svg" slot="item-icon">
+      <img src="../assets/img/tabbar/shopcart_active.svg" slot="item-icon-active">
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile" activeColor="cornflowerblue">
+      <img src="../assets/img/tabbar/profile.svg" slot="item-icon">
+      <img src="../assets/img/tabbar/profile_active.svg" slot="item-icon-active">
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+import TabBar from "./tabbar/TabBar";
+import TabBarItem from "./tabbar/TabBarItem";
+
+export default {
+  name: "MainTabBar",
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+<style scoped>
+
+</style>
